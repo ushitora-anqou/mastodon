45a396ae4b086ac245277de483717ea693c82bbc
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2018-06-05 20:20:23 +0900

    search only stemmed text

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -28,7 +28,7 @@ class SearchService < BaseService
 
   def perform_statuses_search!
     statuses = StatusesIndex.filter(term: { searchable_by: account.id })
-                            .query(multi_match: { type: 'most_fields', query: query, operator: 'and', fields: %w(text text.stemmed) })
+                            .query(multi_match: { type: 'most_fields', query: query, operator: 'and', fields: %w(text.stemmed) })
                             .limit(limit)
                             .order({created_at: {order: :desc}})
                             .objects

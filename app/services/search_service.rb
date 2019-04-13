4bad2953139da4a2c9aaf63e31c979ceb3684763
Ushitora Anqou <ushitora@anqou.net>
2019-04-13 12:28:54 +0900

    Revert "今度こそ日時順に"
    This reverts commit b81db2966e2a0ddeb7662971b4069e86ce094118.

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -35,7 +35,7 @@ class SearchService < BaseService
   def perform_statuses_search!
     definition = StatusesIndex.filter(term: { searchable_by: @account.id })
                               .query(multi_match: { type: 'most_fields', query: @query, operator: 'and', fields: %w(text.stemmed) })
-                              .order(created_at: { order: 'desc' })
+                              .order(id: { order: 'desc' })
 
 
     if @options[:account_id].present?

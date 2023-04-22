e70e63860190fb7f607518a64cd51dce7115d1f1
Ushitora Anqou <ushitora@anqou.net>
2023-04-22 14:20:58 +0900

    [Scheduler::IndexingScheduler] Ignore import! exceptions

--- app/workers/scheduler/indexing_scheduler.rb
+++ app/workers/scheduler/indexing_scheduler.rb
@@ -16,6 +16,9 @@ class Scheduler::IndexingScheduler
       with_redis do |redis|
         redis.sscan_each("chewy:queue:#{type.name}", count: SCAN_BATCH_SIZE).each_slice(IMPORT_BATCH_SIZE) do |ids|
           type.import!(ids)
+        rescue => e
+          Rails.logger.error("Error while running #{type.name}#import!: #{e}")
+        ensure
           redis.pipelined do |pipeline|
             pipeline.srem("chewy:queue:#{type.name}", ids)
           end

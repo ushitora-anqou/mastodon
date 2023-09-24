66687a9e2ea1f0d52bef168a33a1df035f73b592
Ushitora Anqou <ushitora@anqou.net>
2023-09-24 12:37:18 +0900

    Add multiple web UI styles (default,compact,legacy)
    ref: https://github.com/akane-blue/mastodon/commit/619998afe1d98e55f874f90c6b0d3c651130b38a
    ref: https://github.com/akane-blue/mastodon/commit/250ad2d71ecd71be133990fc6174ec0731840551
    ref: https://github.com/akane-blue/mastodon/commit/0ff156c4c99e8bdfb86afe280327f7ba42882884

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -32,6 +32,7 @@ class UserSettings
     setting :expand_content_warnings, default: false
     setting :display_media, default: 'default', in: %w(default show_all hide_all)
     setting :auto_play, default: false
+    setting :mod_webui_styles, default: 'default', in: %w(default compact legacy)
   end
 
   namespace :notification_emails do

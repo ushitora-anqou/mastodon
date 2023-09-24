0d6b66cd652274f00a88848222c526974c3aa30e
Ushitora Anqou <ushitora@anqou.net>
2023-09-24 15:20:55 +0900

    Use Permalink with href instead of Link to jump to remote pages
    ref: https://github.com/akane-blue/mastodon/commit/f7d3270e2c7d7d8c26585290e07925e79bd87ef8

--- app/javascript/mastodon/components/hashtag.jsx
+++ app/javascript/mastodon/components/hashtag.jsx
@@ -5,7 +5,7 @@ import { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+
 
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
@@ -13,6 +13,8 @@ import { Sparklines, SparklinesCurve } from 'react-sparklines';
 
 import { ShortNumber } from 'mastodon/components/short_number';
 import { Skeleton } from 'mastodon/components/skeleton';
+
+import Permalink from './permalink';
 
 class SilentErrorBoundary extends Component {
 
@@ -58,6 +60,7 @@ export const accountsCountRenderer = (displayNumber, pluralReady) => (
 export const ImmutableHashtag = ({ hashtag }) => (
   <Hashtag
     name={hashtag.get('name')}
+    href={hashtag.get('url')}
     to={`/tags/${hashtag.get('name')}`}
     people={hashtag.getIn(['history', 0, 'accounts']) * 1 + hashtag.getIn(['history', 1, 'accounts']) * 1}
     // @ts-expect-error
@@ -70,12 +73,12 @@ ImmutableHashtag.propTypes = {
 };
 
 // @ts-expect-error
-const Hashtag = ({ name, to, people, uses, history, className, description, withGraph }) => (
+const Hashtag = ({ name, href, to, people, uses, history, className, description, withGraph }) => (
   <div className={classNames('trends__item', className)}>
     <div className='trends__item__name'>
-      <Link to={to}>
+      <Permalink href={href} to={to}>
         {name ? <>#<span>{name}</span></> : <Skeleton width={50} />}
-      </Link>
+      </Permalink>
 
       {description ? (
         <span>{description}</span>
@@ -104,6 +107,7 @@ const Hashtag = ({ name, to, people, uses, history, className, description, with
 
 Hashtag.propTypes = {
   name: PropTypes.string,
+  href: PropTypes.string,
   to: PropTypes.string,
   people: PropTypes.number,
   description: PropTypes.node,

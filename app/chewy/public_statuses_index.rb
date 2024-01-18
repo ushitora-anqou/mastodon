692576b58813a22b400496d5ba39b335a79a82e3
Ushitora Anqou <ushitora@anqou.net>
2024-01-18 19:45:04 +0900

    Add full-text search support for Japanese

--- app/chewy/public_statuses_index.rb
+++ app/chewy/public_statuses_index.rb
@@ -4,6 +4,14 @@ class PublicStatusesIndex < Chewy::Index
   include DatetimeClampingConcern
 
   settings index: index_preset(refresh_interval: '30s', number_of_shards: 5), analysis: {
+    tokenizer: {
+      sudachi_tokenizer: {
+        type: 'sudachi_tokenizer',
+        discard_punctuation: true,
+        split_mode: 'C',
+      },
+    },
+
     filter: {
       english_stop: {
         type: 'stop',
@@ -28,13 +36,17 @@ class PublicStatusesIndex < Chewy::Index
       },
 
       content: {
-        tokenizer: 'standard',
+        char_filter: ['icu_normalizer'],
+        tokenizer: 'sudachi_tokenizer',
+        type: 'custom',
         filter: %w(
           lowercase
-          asciifolding
           cjk_width
-          elision
+          sudachi_part_of_speech
+          sudachi_ja_stop
+          sudachi_baseform
           english_possessive_stemmer
+          asciifolding
           english_stop
           english_stemmer
         ),

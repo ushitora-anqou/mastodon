4a9a816b1722b58dca77acbf466576f66d97982a
Ushitora Anqou <ushitora@anqou.net>
2024-10-12 18:24:16 +0900

    restrict mention notifications from accounts with few followers

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -136,7 +136,7 @@ class NotifyService < BaseService
     private
 
     def blocked_mention?
-      FeedManager.instance.filter?(:mentions, @notification.target_status, @recipient)
+      (@sender.followers_count < ENV.fetch('BLOCKED_MENTION_MIN_FOLLOWERS_COUNT', 1).to_i && !@recipient.following?(@sender)) || FeedManager.instance.filter?(:mentions, @notification.target_status, @recipient)
     end
 
     def from_self?
